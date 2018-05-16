2b9771c65813c9a4d43e9a77eb308b7c0adf01f8
Laura Messa Travin <laura.travin@gmail.com>
2018-05-16 03:17:18 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+
+class School
+  attr_accessor :roster
+  attr_reader :school
+  def initialize(n)
+    @school = n
+    @roster = {}
+  end
+  def add_student(n,g)
+     if self.roster.empty?
+       self.roster = {g => [n]}
+     else
+         if self.roster.keys.include?(g)
+              self.roster[g] << n
+         else
+              self.roster[g] = [n]
+         end
+     end
+  end
+  def grade
+  end
+  def sort
+  end
+end
